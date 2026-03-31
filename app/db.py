ced74dc6c7a9888b0b6e40e302c31b4ef2213a32
0x4bubakar <96263616+0x4bubakar@users.noreply.github.com>
2026-03-31 09:38:22 +0100

    Modified db.py and routes.py

--- app/db.py
+++ app/db.py
@@ -1,4 +1,21 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+try:
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+    print("Connection to DB successful!")
+
+except mysql.connector.Error as err:
+    print(err)
+
 
 def create_user(username, plain_text_password):
     hashed_password = generate_password_hash(plain_text_password)
