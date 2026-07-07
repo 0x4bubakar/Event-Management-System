d646cadb357946227e842e904597498b08a65b0e
0x4bubakar <96263616+0x4bubakar@users.noreply.github.com>
2026-07-07 10:21:58 +0100

    Implemented organiser registration

---
+++ organisers/models.py
@@ -0,0 +1,34 @@
+import db_connector
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def create_organiser(name, email, password):
+    conn = db_connector.get_connection()
+    cursor = conn.cursor()
+
+    query = "SELECT email from user WHERE email = %s"
+    cursor.execute(query, (email,))
+    user_exists = cursor.fetchone()
+    
+    if user_exists:
+        return None, None, "Account already exists, please log in."
+    
+    else:
+        user_query = "INSERT INTO user (name, email, password_hash, role) VALUES(%s, %s, %s, %s)"
+        org_query = "INSERT INTO organiser (user_id) VALUES(%s)"
+        password_hash = generate_password_hash(password)
+        role = "organiser"
+        
+        try:
+            cursor.execute(user_query, (name, email, password_hash, role))
+            user_id = cursor.lastrowid
+            cursor.execute(org_query, (user_id,))
+            conn.commit()
+            return user_id, role, "Organiser created successfully!"
+        
+        except Exception as e:
+            conn.rollback()
+            print(f"Database error: {str(e)}")
+            return None, None, "System error, please try again later."
+        
+        finally:
+            cursor.close()
--- organisers/routes.py
+++ organisers/routes.py
@@ -1,3 +1,27 @@
 from flask import Blueprint, redirect, render_template, url_for, request, flash, session
+from .models import *
 
-organisers_bp = Blueprint('organiser', __name__)
+organisers_bp = Blueprint('org', __name__)
+
+@organisers_bp.route('/org-register', methods=['POST', 'GET'])
+def org_register():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        if name and email and password:
+            user_id, role, message = create_organiser(name, email, password)
+            if user_id:
+                flash(message, "flash-success")
+                session["user_id"] = user_id
+                session["name"] = name
+                session["role"] = role
+                return redirect(url_for('users.dashboard'))
+            else:
+                flash(message, "flash-error")
+            
+        else:
+            flash("One of the fields are missing information. Please fill them in.", "flash-error")
+        
+    return render_template('org-register.html')
